718495264f2ab5e2c8a2c118a34f0ca02e6f26a2
KREMER Hugo <33060853+Metzill@users.noreply.github.com>
2022-12-09 09:22:30 +0100

    refactorisation, margin externalisation

--- Screen/ConfirmationResendMail.js
+++ Screen/ConfirmationResendMail.js
@@ -1,29 +1,32 @@
-import {Image, ScrollView, StyleSheet, Text} from "react-native";
+import { Image, ScrollView, StyleSheet, Text } from "react-native";
 import * as React from "react";
-import {background} from "../style/background";
-import {text} from "../style/text";
-import {ButtonComp} from "../Component/Button";
+import { background } from "../style/background";
+import { text } from "../style/text";
+import { margin } from "../style/margin";
+import { ButtonComp } from "../Component/Button";
 
-export function ConfirmationResendMailScreen({navigation}) {
-    return (
-        <ScrollView style={background.background}>
-            <Image style={styles.image} source={require('../image/snowman.png')}/>
-            <Text style={text.text}>Les mails ont été envoyés avec succès.</Text>
-            <ButtonComp onPress={()=> {navigation.navigate('Consultation');}} text="Continuer" isPrimary={'true'} style={styles.margin}/>
-        </ScrollView>
-    );
+export function ConfirmationResendMailScreen({ navigation }) {
+  return (
+    <ScrollView style={background.background}>
+      <Image style={styles.image} source={require("../image/snowman.png")} />
+      <Text style={text.text}>Les mails ont été envoyés avec succès.</Text>
+      <ButtonComp
+        onPress={() => {
+          navigation.navigate("Consultation");
+        }}
+        text="Continuer"
+        isPrimary={"true"}
+        style={margin.margin}
+      />
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    margin: {
-        marginTop: 16,
-        marginLeft: 30,
-        marginRight: 30,
-    },
-    image: {
-        marginTop: 16,
-        width: 150,
-        height: 301,
-        alignSelf: 'center',
-    },
+  image: {
+    marginTop: 16,
+    width: 150,
+    height: 301,
+    alignSelf: "center",
+  },
 });
--- Screen/Home.js
+++ Screen/Home.js
@@ -1,38 +1,55 @@
-import {Image, ScrollView, Text, StyleSheet} from "react-native";
+import { Image, ScrollView, Text, StyleSheet } from "react-native";
 import * as React from "react";
-import {ButtonComp} from "../Component/Button";
-import {useNavigation} from "@react-navigation/native";
-import {background} from "../style/background";
-import {text} from "../style/text";
+import { ButtonComp } from "../Component/Button";
+import { useNavigation } from "@react-navigation/native";
+import { background } from "../style/background";
+import { text } from "../style/text";
+import { margin } from "../style/margin";
 
 export function HomeScreen() {
-    const navigation = useNavigation();
-    const navigateTo = (screen) => {
-        navigation.navigate(screen);
-    }
-    return (
-        <ScrollView style={background.background} >
-            <Image style={styles.image} source={require('../image/gift.png')}/>
-            <Text style={text.text}>Bienvenue dans notre application d’organisation de secret santa !</Text>
-            <Text style={text.text}>Mais qu’est-ce qu’un secret santa ?</Text>
-            <Text style={text.text}>Il s’agit d’un évènement dans lequel les participants offrent un cadeau à un autre participant choisi au hasard !</Text>
-            <Text style={text.text}>Commencez par organiser le vôtre dès maintenant :</Text>
-            <ButtonComp onPress={()=>{navigateTo('Creation')}} text="Organiser" isPrimary={'true'} style={styles.margin}/>
-            <ButtonComp onPress={()=>{navigateTo('Consultation')}} text="Consulter" isPrimary={'false'} style={styles.margin}/>
-        </ScrollView>
-    );
+  const navigation = useNavigation();
+  const navigateTo = (screen) => {
+    navigation.navigate(screen);
+  };
+  return (
+    <ScrollView style={background.background}>
+      <Image style={styles.image} source={require("../image/gift.png")} />
+      <Text style={text.text}>
+        Bienvenue dans notre application d’organisation de secret santa !
+      </Text>
+      <Text style={text.text}>Mais qu’est-ce qu’un secret santa ?</Text>
+      <Text style={text.text}>
+        Il s’agit d’un évènement dans lequel les participants offrent un cadeau
+        à un autre participant choisi au hasard !
+      </Text>
+      <Text style={text.text}>
+        Commencez par organiser le vôtre dès maintenant :
+      </Text>
+      <ButtonComp
+        onPress={() => {
+          navigateTo("Creation");
+        }}
+        text="Organiser"
+        isPrimary={"true"}
+        style={margin.margin}
+      />
+      <ButtonComp
+        onPress={() => {
+          navigateTo("Consultation");
+        }}
+        text="Consulter"
+        isPrimary={"false"}
+        style={margin.margin}
+      />
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    margin: {
-        marginTop: 16,
-        marginLeft: 30,
-        marginRight: 30,
-    },
-    image: {
-        marginTop: 16,
-        width: 343,
-        height: 215,
-        alignSelf: 'center',
-    },
+  image: {
+    marginTop: 16,
+    width: 343,
+    height: 215,
+    alignSelf: "center",
+  },
 });

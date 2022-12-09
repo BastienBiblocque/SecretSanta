7ee5835239aa0cbff95e43a80f59c98847a82d1b
Bastien <45488913+BastienBiblocque@users.noreply.github.com>
2022-12-09 16:25:56 +0100

    formulaire de création de secret santa

--- utils/checkParticipant.js
+++ utils/checkParticipant.js
@@ -1,13 +1,12 @@
-export const checkParticipant = (evenement) => {
-    const allPlayerName = evenement.player.map((player) => player.name);
+export const checkParticipant = (players) => {
+    const allPlayerName = players.map((player) => player.name);
     if (allPlayerName.length !== new Set(allPlayerName).size) {
         return {title: 'Nom similaire', message: "Les noms des participants doivent être différents"};
     }
-    const allPlayerEmail = evenement.player.map((player) => player.email);
+    const allPlayerEmail = players.map((player) => player.email);
     if (allPlayerEmail.length !== new Set(allPlayerEmail).size) {
         return {title: 'Mail similaire', message: "Les mails des participants doivent être différents"};
     }
-    console.log(allPlayerName.length)
     if (allPlayerName.length < 3) {
         return {
             title: 'Nombre insufisant',
